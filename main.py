bad209683e99bab1f841e29e684b1dfb285d0855
Adityamane10 <117142655+Adityamane10@users.noreply.github.com>
2026-02-22 18:53:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,12 +11,9 @@ load_dotenv()
 
 app = FastAPI()
 
-cors_origins_str = os.getenv("CORS_ORIGINS", "http://localhost:5173")
-origins = [origin.strip() for origin in cors_origins_str.split(",")]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
